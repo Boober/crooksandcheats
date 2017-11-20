a48a75b4fc9ec64a66749ebed6a17c867b8e473f
Randy D. Epouhe <repouhe@andrew.cmu.edu>
2017-11-20 10:23:16 -0500

    Replicated actions for abilties and traps models.

---
+++ models/traps.js
@@ -0,0 +1,98 @@
+
+var mongoClient = require('mongodb').MongoClient;
+
+var connection_string = 'mongodb://localhost:27017/crooksdb';
+
+/*
+* If there is an MLAB password, we should use it instead of the local one.
+*/
+
+console.log("Testing connection...");
+if (process.env.MLAB_CROOKSDB_PASSWD)
+{
+	console.log("successful adding external connection...");
+	connection_string = "mongodb://admin:"
+						+ process.env.MLAB_CROOKSDB_PASSWD
+						+ "@ds155820.mlab.com:55820/crooksdb";
+}
+
+
+//Global var for connected database
+
+var mongoDB;
+
+console.log(connection_string);
+
+mongoClient.connect(connection_string, function(err,db) {
+	if (err) doError(err);
+	console.log("Connected to MongoDB server.");
+	mongoDB = db;
+});
+
+
+/****************** CRUD Create -> Mongo Insert ********************/
+
+exports.create = function(data,callback)
+{
+	mongoDB.collection("traps").insertOne(
+		data,
+		function(err,status)
+		{
+			if (err) doError(err);
+			var success = (status.result.n == 1 ? true : false);
+			callback(success);
+			console.log("Insert into Traps successful.");
+		});
+}
+
+
+/*************** CRUD Retrieve -> Mongo Find ***********************/
+
+exports.retrieve = function(query,callback)
+{
+	mongoDB.collection("traps").find(query).toArray(function(err,docs)
+	{
+		if (err) doError(err);
+		console.log("No errors, performing callback.");
+		callback(docs);
+	})
+}
+
+
+/*************** CRUD Update -> Mongo updateMany *********************/
+
+exports.update = function(filter,update,callback)
+{
+	mongoDB.collection("traps").updateMany(
+		filter,
+		update,
+		{upsert:true},
+
+		function(err,status) {
+			if (err) doError(err);
+			callback('Modified ' + status.modifiedCount + ' and added ' + status.upsertedCount + " documents");
+		});
+}
+
+
+/************** CRUD Delete -> Mongo deleteMany *********************/
+
+exports.delete = function(query, callback)
+{
+	mongoDB.collection("traps").deleteMany(
+		query,
+		function(err,status)
+		{
+			if (err) throw err;
+			callback(status.result.n + "document(s) have been removed.");
+		});
+}
+
+
+
+
+var doError = function(e)
+{
+	console.error("Error: " + e);
+	throw new Error(e);
+}
---
+++ routes/abilities.js
@@ -0,0 +1,88 @@
+
+
+//include the model
+var model = require("../models/monsters.js");
+
+//Define the collection name.
+let cname = "abilities";
+
+exports.init = function(app)
+{
+	app.put("/" + cname, doCreate);
+	app.get("/" + cname, doRetrieve);
+	app.post("/" + cname, doUpdate);
+	app.delete("/" + cname, doDelete);
+}
+
+
+doCreate = function(req,res)
+{
+	if (Object.keys(req.body).length == 0)
+	{
+		res.render('message', {title: 'monsters', obj: "No create message body found"});
+		return;
+	}
+
+	//Restrict the parameters to the ones we expect. 9Don't allow user to add properties outside of what we expect.
+	else if (Object.keys(req.body).length != 4 &&
+		(!req.body.name || !req.body.targets || !req.body.description || !req.body.effect))
+	{
+		res.render('message', {title: 'monsters', obj: "Invalid parameters. (Need name, description, targets, effect.)"});
+		return;
+	}
+
+
+	model.create(req.body,
+							function(result) {
+								var success = (result ? "Create successful" : "Create unsuccessful");
+								res.render('message', {title: 'monsters', obj: success});
+							});
+}
+
+
+doRetrieve = function(req,res)
+{
+	model.retrieve(req.query,
+								function(modelData) {
+									if (modelData.length)
+									{
+										console.log("Rendering results.");
+										res.render('results',{title: 'monsters', obj: modelData});
+
+									} else {
+										console.log("No documents.");
+										var message = "No documents with" + JSON.stringify(req.query)+
+														" in collection " + cname + " found.";
+										res.render('message', {title: 'monsters', obj: message});			
+									}
+								});
+}
+
+
+doUpdate = function(req,res)
+{
+	var filter = req.body.find ? JSON.parse(req.body.find) : {};
+
+	if (!req.body.update)
+	{
+		res.render('message', {title: 'monsters', obj: "No update operation defined."});
+	}
+
+	var update = JSON.parse(req.body.update);
+
+	model.update(filter, update,
+					function(status) {
+						res.render('message',{title: 'monsters', obj: status});
+					});
+}
+
+
+doDelete = function(req,res)
+{
+	var query = req.body.delete ? JSON.parse(req.body.delete) : {};
+
+	model.delete(query,
+				 	function(status) {
+				 		res.render('message', {title: 'monsters', obj: status});
+				 	});
+}
